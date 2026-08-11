d59961d1173c0569eaade0c3a7d9d1a9365b295d
vaishnavim01 <morevaishnavivinayak@gmail.com>
2026-08-11 13:17:56 +0530

    Add files via upload

---
+++ lab1 exercise 2.py
@@ -0,0 +1,13 @@
+#Employee Profile Generator
+
+name=input("Enter Employee Name:")
+role=input("Enter Employee Role:")
+monthly_salary=float(input("Enter Employee Monthly Salary:"))
+
+print("\n*******")
+print("\n---Employee Identity Card---\n")
+print("*******")
+
+print("Name:",name)
+print("Role:",role)
+print("Salary", monthly_salary)
